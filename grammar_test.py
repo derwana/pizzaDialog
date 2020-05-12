f39a86184b0a9e246dc4c54997f3e9657307afc6
Christoph Werner <christoph@werners.name>
2020-05-12 15:16:40 +0200

    added sentence tree parsing

--- grammar_test.py
+++ grammar_test.py
@@ -1,10 +1,18 @@
 from nltk.parse.generate import generate
 from nltk import CFG
+import nltk
 
 with open('grammar', 'r') as file:
     grammar_data = file.read()
 
 grammar = CFG.fromstring(grammar_data)
 
-for sentence in generate(grammar, depth=6):
-     print(' '.join(sentence))
+#for sentence in generate(grammar, depth=4, n=10):
+#    print(' '.join(sentence))
+
+parser = nltk.ChartParser(grammar)
+
+sentence = ['Pizza','Salami','mit','extra','Salami']
+
+for tree in parser.parse(sentence):
+    print(tree)
